ee902507e3ad440eb8971e35d67155dbce325319
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-07-14 21:24:28 +0800

    Write the files to cache.

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
+
+const FILES_TO_CACHE = [
+    "/",
+    "/js/db.js",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+];
